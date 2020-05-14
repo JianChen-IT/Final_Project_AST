4eb0086894573b6ec42b74f859fa64185c0ba82f
JianoChen <jian.chen01@estudiant.upf.edu>
2020-05-14 18:35:59 +0200

    Functional Trivial

---
+++ main.py
@@ -0,0 +1,22 @@
+# 1. Lectura de base de datos
+# 2. Display --> pregunta + opciones (a,b,c,d || true,false & repeat command)
+# 3. TTS para leer
+# 4. Grabar la respuesta de un jugador e implementar un timer
+# 5. Chequear pregunta
+
+import question_reader as qr
+
+while(1):
+    filename = qr.pick_category()
+    correct_answer,possible_answers = qr.read_json(filename)
+    options = {}
+    available_options = ["a","b","c","d"]
+    for i in range(len(available_options)):
+        options[available_options[i]]=i
+
+    print("Pick and answer: ", end="")
+    user_answer = input()
+    if(user_answer =="exit"):
+        break
+    qr.check_solution(correct_answer, possible_answers[options[user_answer]])
+    
---
+++ question_reader.py
@@ -0,0 +1,43 @@
+import os
+import random
+import json
+import html
+
+os.chdir(os.path.dirname(os.path.abspath(__file__)) + "/Database/")
+def pick_category():
+    #categories_file = os.path.dirname(os.path.abspath(__file__)) + "/Database/"
+    all_categories = os.listdir()
+    filename = random.choice(all_categories)
+    #print(all_categories)
+    return filename
+
+def read_json(my_json):
+    json_file = open(os.getcwd() + "/" + my_json, 'r')
+    data = json.load(json_file)
+    choice = random.randint(0, len(data["results"]))
+    output_scaped = html.unescape(data["results"][choice]["question"])
+
+    correct_answer = data["results"][choice]["correct_answer"]
+
+    possible_answers = data["results"][choice]["incorrect_answers"]
+    possible_answers.append(correct_answer)
+    for i in range(len(possible_answers)):
+        possible_answers[i] = html.unescape(possible_answers[i])
+           
+    random.shuffle(possible_answers)
+    print(output_scaped)
+    options = ["a","b","c","d"]
+    for i in range(len(possible_answers)):
+            print("\t"+options[i] + "." + " " + possible_answers[i] + "\t")
+    
+    json_file.close()
+    return correct_answer,possible_answers
+
+def check_solution(correct_answer, given_answer):
+    if correct_answer == given_answer:
+        print("Correct!")
+        return True
+    else:
+        print("Incorrect! The correct answer is: " + correct_answer)
+        return False
+
